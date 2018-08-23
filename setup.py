ca6e08235fb7fb8b6ee0dec2d3b66a4dd62a8731
Adam Svanberg <asvanberg@gmail.com>
2018-08-23 15:18:41 +0200

    Python packaging structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='libdrf',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Reusable apps and utilities for Django Rest Framework projects',
+    url='https://github.com/lumiqa/libdrf',
+    author='Adam Svanberg',
+    author_email='adam@lumiqa.com',
+)
