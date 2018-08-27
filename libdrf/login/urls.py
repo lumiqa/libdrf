308ed5f020de83e40e489151dd78bb2fd3585f94
Adam Svanberg <asvanberg@gmail.com>
2018-08-27 15:02:24 +0200

    new urls format

--- libdrf/login/urls.py
+++ libdrf/login/urls.py
@@ -1,25 +1,25 @@
-from django.conf.urls import url
+from django.urls import path
 from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    url(r'^user', views.UserView.as_view(), name='current-user'),
+    path(r'user', views.UserView.as_view(), name='current-user'),
 
     # JWT auth
-    url(r'^login$', views.LoginView.as_view(), name='login'),
-    url(r'^logout$', views.LogoutView.as_view(), name='logout'),
-    url(r'^register$', views.RegistrationView.as_view(), name='register'),
-    url(r'^unregister$', views.UnregistrationView.as_view(), name='unregister'),
-    url(r'^register/resend$', views.ResendActivationView.as_view(), name='resend-activation'),
-    url(r'^activate$', views.ActivationView.as_view(), name='activate'),
-    url(r'^password/reset$', views.ResetPasswordView.as_view(), name='reset-password'),
-    url(r'^password$', views.ChangePasswordView.as_view(), name='change-password'),
+    path(r'login', views.LoginView.as_view(), name='login'),
+    path(r'logout', views.LogoutView.as_view(), name='logout'),
+    path(r'register', views.RegistrationView.as_view(), name='register'),
+    path(r'unregister', views.UnregistrationView.as_view(), name='unregister'),
+    path(r'register/resend', views.ResendActivationView.as_view(), name='resend-activation'),
+    path(r'activate', views.ActivationView.as_view(), name='activate'),
+    path(r'password/reset', views.ResetPasswordView.as_view(), name='reset-password'),
+    path(r'password', views.ChangePasswordView.as_view(), name='change-password'),
 
     # Social token exchange
-    url(r'^validate/google$', views.GoogleTokenExhangeView.as_view(), name='validate_google_token'),
-    url(r'^validate/facebook$', views.FacebookTokenExhangeView.as_view(), name='validate_facebook_token'),
+    path(r'validate/google', views.GoogleTokenExhangeView.as_view(), name='validate_google_token'),
+    path(r'validate/facebook', views.FacebookTokenExhangeView.as_view(), name='validate_facebook_token'),
 
     # Rendered email templates
-    url(r'^email/activation$', TemplateView.as_view(template_name='login/email-activation-inline.html')),
+    path(r'email/activation', TemplateView.as_view(template_name='login/email-activation-inline.html')),
 ]
