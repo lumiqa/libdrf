b9a68b96ba7e46d148484c926c6202a9e61f825f
Adam Svanberg <asvanberg@gmail.com>
2019-02-01 16:24:17 +0100

    logsql middleware

---
+++ libdrf/middleware.py
@@ -0,0 +1,46 @@
+import logging
+from collections import Counter
+from operator import itemgetter
+from pprint import pformat
+
+from django.db import connection
+from django.test import override_settings
+
+logger = logging.getLogger(__name__)
+
+
+class LogSQLMiddleware:
+
+    calls = Counter()
+    queries = Counter()
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """Log the SQL queries that the request ran"""
+        with override_settings(DEBUG=True):
+            response = self.get_response(request)
+        logger.info(
+            "SQL Queries for request {}:\n{}".format(
+                request.path, pformat(connection.queries)
+            )
+        )
+        logger.info(
+            "Total SQL Queries for request {}: {}".format(
+                request.path, len(connection.queries)
+            )
+        )
+        self.calls[request.path] += 1
+        self.queries[request.path] += len(connection.queries)
+        averages = [
+            (ep, self.queries[ep] / float(self.calls[ep]) if self.calls[ep] else 0.0)
+            for ep in self.queries
+        ]
+        averages.sort(key=itemgetter(1))
+        logger.info(
+            "Endpoint averages:\n{}".format(
+                "\n".join("{}: {}".format(ep, avg) for ep, avg in averages)
+            )
+        )
+        return response
